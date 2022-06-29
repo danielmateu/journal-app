b556864a74c2a0bd0d2daf7ea5190b11b1295970
Daniel Mateu <danielmateu86@gmail.com>
2022-06-29 15:26:15 +0200

    Firest Commit

---
+++ src/JournalApp.jsx
@@ -0,0 +1,10 @@
+
+
+export const JournalApp = () => {
+  return (
+    <>
+    
+    <h1>JournalApp</h1>
+    </>
+  )
+}
---
+++ src/README.md
@@ -0,0 +1,5 @@
+# README
+
+Instalamos la version 6 del react-router-dom:
+
+yarn add react-router-dom@6
